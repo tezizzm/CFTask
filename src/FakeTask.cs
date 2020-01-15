a430749494aabb298ad3c0cb6a8c8f1fbc159cdf
tezizzm <martezkillens@gmail.com>
2020-01-15 13:26:52 -0700

    Initial checkin.

---
+++ src/FakeTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Steeltoe.Common.Tasks;
+
+namespace task
+{
+    public class FakeTask : IApplicationTask
+    {
+        private static int _invocations;
+
+        public void Run()
+        {
+            _invocations++;
+            Console.WriteLine($"The task has been invoked {_invocations} times");
+        }
+
+        public string Name => "FakeTask";
+    }
+}
